9cf0f309486478c2857b7c190419b64220ce3955
Test User <test@example.com>
2026-06-08 15:13:29 +0200

    Import Android download stream classes

--- android/src/org/rockbox/Helper/Connectivity.java
+++ android/src/org/rockbox/Helper/Connectivity.java
@@ -12,6 +12,8 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
